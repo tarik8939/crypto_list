d1f6c14e7c78e0498ececd0531a1d9d322d193a4
Taras Horilchanyi <39026353+tarik8939@users.noreply.github.com>
2023-11-09 16:54:10 +0200

    Added DI

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -1,10 +1,18 @@
-import 'package:crypto_list/repositories/crypto_coins/crypto_coins.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
 
+import 'package:crypto_list/repositories/crypto_coins/crypto_coins.dart';
+
 class CryptoCoinsRepository extends AbstractCoinsRepository {
+  CryptoCoinsRepository({
+    required this.dio,
+  });
+
+  final Dio dio;
+
   @override
   Future<List<CryptoCoin>> getCoinsList() async {
-    final responce = await Dio().get(
+    final responce = await dio.get(
         'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,AVAX,AID,SOL,CAG,DOV&tsyms=USD');
 
     final data = responce.data as Map<String, dynamic>;
